0cdb846212dcb3747cac3c61b9ce8169fae81c9c
Shyam Sundar <shyamsundar19193@gmail.com>
2026-01-20 13:15:36 +0530

    add studentModel

---
+++ src/main/java/com/example/day3SMS/model/StudentModel.java
@@ -0,0 +1,16 @@
+package com.example.day3SMS.model;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "student")
+public class StudentModel {
+    @Id
+    private String id;
+    private String name;
+    private int age;
+    private String email;
+
+}
