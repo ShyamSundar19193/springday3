3d87b2fd32c18a24389bd5896a2684205d4d5ae3
Shyam Sundar <shyamsundar19193@gmail.com>
2026-01-20 11:53:46 +0530

    initial commit

---
+++ src/test/java/com/example/day3SMS/Day3SmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.day3SMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day3SmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
